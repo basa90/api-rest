8a819d657ade0dd56a9a3c55dd2ed114670694ae
Gabriel Nunez <gabriel.nunez.sme@outlook.com>
2017-03-05 18:47:12 -0400

    #11 Refactorizando el codigo del proyecto

---
+++ controllers/product.js
@@ -0,0 +1,95 @@
+'use strict'
+
+const Product = require('../models/product')
+
+function getProduct(req, res) {
+	let productId = req.params.productId
+
+	Product.findById(productId)
+	.then((product) => {
+		if(!product){
+			return res.status(404).send({message: `El producto no existe`})
+		}
+
+		res.status(200).send({product})
+	})
+	.catch((err) => {
+		return res.status(500).send({message: `Error al realizar la peticion: ${err}`})
+	})
+}
+
+function getProducts(req, res) {
+	Product.find({})
+	.then((products) => {
+		if(!products){
+			return res.status(404).send({message: `No existen productos`})
+		}
+
+		res.status(200).send({products})
+	})
+	.catch((err) => {
+		return res.status(500).send({message: `Error al realizar la peticion: ${err}`})
+	})
+}
+
+function saveProduct(req, res) {
+	let product = new Product()
+
+	product.name = req.body.name
+	product.picture = req.body.picture
+	product.price = req.body.price
+	product.category = req.body.category
+	product.description = req.body.description
+
+	product.save()
+	.then((newProduct) => {
+		res.status(200).send({newProduct})
+	})
+	.catch((err) => {
+		return res.status(500).send({message: `Error al guardar en la base de datos: ${err}`})
+	})
+}
+
+function updateProduct(req, res) {
+	let productId = req.params.productId
+	let update = req.body
+
+	Product.findByIdAndUpdate(productId, update)
+	.then((updatedProduct) => {
+		if(!updatedProduct){
+			return res.status(404).send({message: `El producto no existe`})
+		}
+
+		res.status(200).send({updatedProduct})
+	})
+	.catch((err) => {
+		return res.status(500).send({message: `Error al actualizar: ${err}`})
+	})
+}
+
+function deleteProduct(req, res) {
+	let productId = req.params.productId
+
+	Product.findById(productId)
+	.then((product) => {
+		if(!product){
+			return res.status(404).send({message: `El producto no existe`})
+		}
+
+		return product.remove()
+	})
+	.then((deletedProduct) => {
+		res.status(200).send({deletedProduct})
+	})
+	.catch((err) => {
+		return res.status(500).send({message: `Error al eliminar: ${err}`})
+	})
+}
+
+module.exports = {
+	getProduct,
+	getProducts,
+	saveProduct,
+	updateProduct,
+	deleteProduct,
+}
--- index.js
+++ index.js
@@ -1,110 +1,17 @@
 'use strict'
 
-const express = require('express')
 let mongoose = require('mongoose')
 //estableciendo bluebird como el proveedor de promises de mongoose
 mongoose.Promise = require('bluebird')
+const app = require('./app')
+const config = require('./config')
 
-const Product = require('./models/product')
-
-const bodyParser = require('body-parser')
-const app = express()
-const port = process.env.PORT || 3001
-
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-app.get('/api/product', (req, res) => {
-	Product.find({})
-	.then((products) => {
-		if(!products){
-			return res.status(404).send({message: `No existen productos`})
-		}
-
-		res.status(200).send({products})
-	})
-	.catch((err) => {
-		return res.status(500).send({message: `Error al realizar la peticion: ${err}`})
-	})
-})
-
-app.get('/api/product/:productId', (req, res) => {
-	let productId = req.params.productId
-
-	Product.findById(productId)
-	.then((product) => {
-		if(!product){
-			return res.status(404).send({message: `El producto no existe`})
-		}
-
-		res.status(200).send({product})
-	})
-	.catch((err) => {
-		return res.status(500).send({message: `Error al realizar la peticion: ${err}`})
-	})
-})
-
-app.post('/api/product', (req, res) => {
-	let product = new Product()
-
-	product.name = req.body.name
-	product.picture = req.body.picture
-	product.price = req.body.price
-	product.category = req.body.category
-	product.description = req.body.description
-
-	product.save()
-	.then((newProduct) => {
-		res.status(200).send({newProduct})
-	})
-	.catch((err) => {
-		return res.status(500).send({message: `Error al guardar en la base de datos: ${err}`})
-	})
-})
-
-
-app.put('/api/product/:productId', (req, res) => {
-	let productId = req.params.productId
-	let update = req.body
-
-	Product.findByIdAndUpdate(productId, update)
-	.then((updatedProduct) => {
-		if(!updatedProduct){
-			return res.status(404).send({message: `El producto no existe`})
-		}
-
-		res.status(200).send({updatedProduct})
-	})
-	.catch((err) => {
-		return res.status(500).send({message: `Error al actualizar: ${err}`})
-	})
-})
-
-app.delete('/api/product/:productId', (req, res) => {
-	let productId = req.params.productId
-
-	Product.findById(productId)
-	.then((product) => {
-		if(!product){
-			return res.status(404).send({message: `El producto no existe`})
-		}
-
-		return product.remove()
-	})
-	.then((deletedProduct) => {
-		res.status(200).send({deletedProduct})
-	})
-	.catch((err) => {
-		return res.status(500).send({message: `Error al eliminar: ${err}`})
-	})
-})
-
-mongoose.connect('mongodb://localhost:27017/shop')
+mongoose.connect(config.db)
 .then((res) => {
 	console.log('Conexion a la base de datos establecida...')
 
-	app.listen(port, () => {
-		console.log(`API REST corriendo en http://localhost:${port}`)
+	app.listen(config.port, () => {
+		console.log(`API REST corriendo en http://localhost:${config.port}`)
 	})
 })
 .catch((err) => {
