50daf1d66b13b7a757597fb148510af95d4f84ca
Gabriel Nunez <gabriel.nunez.sme@outlook.com>
2017-03-06 12:43:58 -0400

    #15 Protegiendo rutas con webtokens

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@
 
 const express = require('express')
 const productCtrl = require('../controllers/product')
+const auth = require('../middlewares/auth')
 const api = express.Router()
 
+api.use(auth.isAuth)
+api.get('/private', function (req, res) {
+	res.status(200).send({message: 'Tienes acceso'})
+})
 api.get('/product', productCtrl.getProducts)
 api.get('/product/:productId', productCtrl.getProduct)
 api.post('/product', productCtrl.saveProduct)
